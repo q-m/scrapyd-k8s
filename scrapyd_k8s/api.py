3b2ecc1d6bde78805f24b4bac1bf8398cfa8f916
wvengen <willem@thequestionmark.org>
2024-02-09 14:56:46 +0100

    Better handling of missing query and form parameters (#2)

--- scrapyd_k8s/api.py
+++ scrapyd_k8s/api.py
@@ -22,10 +22,12 @@ def api_daemonstatus():
 
 @app.post("/schedule.json")
 def api_schedule():
-    project_id = request.form['project']
+    project_id = request.form.get('project')
+    if not project_id: return error('project missing in form parameters')
     project = config.project(project_id)
-    if not project: return error('project not found')
-    spider = request.form['spider']
+    if not project: return error('project not found in configuration')
+    spider = request.form.get('spider')
+    if not spider: return error('spider not found in form parameters')
     settings = dict(x.split('=', 1) for x in request.form.getlist('setting'))
     job_id = request.form.get('jobid', uuid.uuid1().hex)
     # priority = request.form.get('priority') or 0 # TODO implement priority
@@ -38,8 +40,10 @@ def api_schedule():
 
 @app.post("/cancel.json")
 def api_cancel():
-    project_id = request.form['project']
-    job_id = request.form['job']
+    project_id = request.form.get('project')
+    if not project_id: return error('project missing in form parameters')
+    job_id = request.form.get('job')
+    if not job_id: return error('job missing in form parameters')
     signal = request.form.get('signal', 'TERM')
     # TODO get job state and return in prevstate
     prevstate = launcher.cancel(project_id, job_id, signal)
@@ -52,9 +56,10 @@ def api_listprojects():
 
 @app.get("/listversions.json")
 def api_listversions():
-    project_id = request.args['project']
+    project_id = request.args.get('project')
+    if not project_id: return error('project missing in query parameters')
     project = config.project(project_id)
-    if not project: return error('project not found')
+    if not project: return error('project not found in configuration')
     tags = repository.listtags(project['repository'])
     tags = [t for t in tags if not t.startswith('sha-')]
     tags.sort(key=natsort_keygen(alg=ns.NUMAFTER))
@@ -62,9 +67,10 @@ def api_listversions():
 
 @app.get("/listspiders.json")
 def api_listspiders():
-    project_id = request.args['project']
+    project_id = request.args.get('project')
+    if not project_id: return error('project missing in query parameters')
     project = config.project(project_id)
-    if not project: return error('project not found')
+    if not project: return error('project not found in configuration')
     _version = request.args.get('_version', 'latest') # TODO allow customizing latest tag
     spiders = repository.listspiders(project['repository'], project_id, _version)
     return { 'status': 'ok', 'spiders': spiders }
