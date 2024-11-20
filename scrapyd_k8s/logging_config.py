d58776bf4f585fc68550cd25d1a9e5b3410a84bc
Valeriia Klestova <v.s.apukhtina@gmail.com>
2024-11-20 14:14:15 +0100

    add logging.getLevelName() method to be used instead of creating a dict with all available logging level to validate user input from the config file

--- scrapyd_k8s/logging_config.py
+++ scrapyd_k8s/logging_config.py
@@ -1,29 +1,18 @@
 import logging
 import sys
 
-VALID_LOG_LEVELS = {
-    'CRITICAL': logging.CRITICAL,
-    'ERROR': logging.ERROR,
-    'WARNING': logging.WARNING,
-    'INFO': logging.INFO,
-    'DEBUG': logging.DEBUG,
-    'NOTSET': logging.NOTSET,
-}
-
 def setup_logging(logging_level):
     if not logging_level:
         logging_level = 'INFO'  # Default to INFO if logging_level is None
 
     level_name = str(logging_level).upper()
-
-    if level_name not in VALID_LOG_LEVELS:
-        valid_levels_str = ', '.join(VALID_LOG_LEVELS.keys())
+    numeric_level = logging.getLevelName(level_name)
+    if not isinstance(numeric_level, int):
         raise ValueError(
-            f"Invalid logging level '{logging_level}'. Valid levels are: {valid_levels_str}"
+            f"Invalid logging level '{logging_level}'."
         )
-
     logging.basicConfig(
-        level=VALID_LOG_LEVELS[level_name],
+        level=numeric_level,
         format='%(asctime)s %(name)s [%(levelname)s]: %(message)s',
         handlers=[logging.StreamHandler(sys.stdout)]
     )
